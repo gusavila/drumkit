c29990dd37e5de99ca740be27b6bc445dacf870b
Gustavo Ávila <71139006+gusavila@users.noreply.github.com>
2025-06-03 01:32:52 -0300

    refactor: rewrite JS using arrow functions and improve code readability
    Updated event listeners and functions in the Drum Kit project to use arrow functions. Code is now cleaner and more modern.

--- index.js
+++ index.js
@@ -1,80 +1,53 @@
-var numberOfDrumsButtons = document.querySelectorAll(".drum").length;
+var drumButtons = document.querySelectorAll(".drum");
 
-//Detecting button press
+drumButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    var key = this.querySelector("span").innerText.toLowerCase();
+    makeSound(key);
+    buttonAnimation(key);
+  });
+});
 
-for (var i = 0; i<numberOfDrumsButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        
-        var buttonInnerHTML = this.querySelector("span").innerText;
-
-        makeSound(buttonInnerHTML);   
-
-        buttonAnimation(buttonInnerHTML);
-
-    });
-    
-}
-
-//Detecting keyboard press
-
-document.addEventListener("keydown", function(event) {
-
-    makeSound(event.key);
-
-    buttonAnimation(event.key);
-
+document.addEventListener("keydown", (event) => {
+  makeSound(event.key.toLowerCase());
+  buttonAnimation(event.key.toLowerCase());
 });
 
 function makeSound(key){
-
-    switch (key) {
-        case "w":
-            var audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-            break;
-
-        case "e":
-            var audio = new Audio("sounds/tom-2.mp3");
-            audio.play();
-            break;
-        
-        case "r":
-            var audio = new Audio("sounds/tom-3.mp3");
-            audio.play();
-            break;
-
-        case "a":
-            var audio = new Audio("sounds/crash.mp3");
-            audio.play();
-            break;
-            
-        case "s":
-            var audio = new Audio("sounds/snare.mp3");
-            audio.play();
-            break;
-            
-        case "d":
-            var audio = new Audio("sounds/kick-bass.mp3");
-            audio.play();
-            break;
-
-        case "f":
-            var audio = new Audio("sounds/tom-4.mp3");
-            audio.play();
-            break;
-
-        default:
-            break;
-    }
-};
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "e":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "r":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "f":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/snare.mp3").play();
+      break;
+    case "a":
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+    default:
+      console.log("Nenhum som para:", key);
+      break;
+  }
+}
 
 function buttonAnimation(currentKey) {
-
-    var activeButton = document.querySelector("." + currentKey);
-    
-    activeButton.classList.add("pressed");
-
-    setTimeout(function() {
-        activeButton.classList.remove("pressed");
-    }, 100);
-};
+  var activeButton = document.querySelector("." + currentKey);
+  if (!activeButton) return;
+  
+  activeButton.classList.add("pressed");
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
