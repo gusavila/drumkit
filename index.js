2007e1a4367b2e09389558f271bc260cd3299be7
Gustavo Ávila <71139006+gusavila@users.noreply.github.com>
2025-06-11 22:29:25 -0300

    feat: Remove 'tom4' and change the buttons order

--- index.js
+++ index.js
@@ -15,25 +15,22 @@ document.addEventListener("keydown", (event) => {
 
 function makeSound(key){
   switch (key) {
+    case "q":
+      new Audio("sounds/crash.mp3").play();
+      break;
     case "w":
       new Audio("sounds/tom-1.mp3").play();
       break;
     case "e":
       new Audio("sounds/tom-2.mp3").play();
       break;
-    case "r":
+    case "d":
       new Audio("sounds/tom-3.mp3").play();
       break;
-    case "f":
-      new Audio("sounds/tom-4.mp3").play();
-      break;
-    case "s":
+    case "a":
       new Audio("sounds/snare.mp3").play();
       break;
-    case "a":
-      new Audio("sounds/crash.mp3").play();
-      break;
-    case "d":
+    case "s":
       new Audio("sounds/kick-bass.mp3").play();
       break;
     default:
